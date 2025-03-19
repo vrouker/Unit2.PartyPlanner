853f2b38ef6192c3e42d08ce7c5d3dbf4392ead5
vrouker <vrouker@gmail.com>
2025-03-18 22:09:21 -0400

    created the html and js files. added the user story and starting working through the pieces

---
+++ index.js
@@ -0,0 +1,8 @@
+/**User Story:
+ * A user enters the website and finds a list of the names, dates, times, locations, and descriptions of all the parties that are happening. -> pulled from the API
+ * Next to each party in the list is a delete button. The user clicks the delete button for one of the parties. -> click event listener
+ * That party is then removed from the list. -> fetch delete
+ * There is also a form that allows the user to enter information about a new party that they want to schedule. outline form in HTML with ids -> add functionality to save the input in the JS -> submit event listener
+ * After filling out the form and submitting it, the user observes their party added to the list of parties. rerender the updated information -> mockAPI? */
+
+//Step 1:
